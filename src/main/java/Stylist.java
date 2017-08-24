2d8a5549f99dfc5a380a8cf40af37cd1f59bdb3c
Alexander <mburukungu@gmail.com>
2017-08-24 15:22:41 +0300

    place method to get name of stylist

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,11 @@
+public class Stylist {
+  private String name;
+
+  public Stylist(String name, String description) {
+    mName = name;
+
+  }
+
+//get the name of the stylist
+  public String getName() {
+    return name;
