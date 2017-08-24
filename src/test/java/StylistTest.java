5ebc410b4b2d86475f53c1ec485a7cac9e4cca78
Alexander <mburukungu@gmail.com>
2017-08-24 18:42:00 +0300

    test2 - check if name of stylist returns as a string

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -3,10 +3,16 @@ import static org.junit.Assert.*;
 
 public class StylistTest {
 
-  //check if tests instatiates correctly
-  @Test
+    //check if tests instatiates correctly
+    @Test
     public void stylist_instantiatesCorrectly_true() {
       assertTrue(stylist instanceof Stylist);
+    }
+
+    //check if name of stylist returns as a string
+    @Test
+    public void getName_returnsNameOfStylist_String() {
+    assertEquals("Xena", stylist.getName());
     }
 
 }
