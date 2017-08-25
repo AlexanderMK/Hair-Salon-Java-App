be7841a90c957b3a018c52933f183281a723e092
Alexander <mburukungu@gmail.com>
2017-08-25 16:16:38 +0300

    place method to give stylist objects a name property

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,8 +1,16 @@
+
+
 public class Stylist {
+
   private String name;
 
-
+  //logic for initial instantiation
   public Stylist(String name) {
     mName = name;
+  }
+
+  //give a stylist a name
+  public String getName() {
+    return mName;
   }
 }
