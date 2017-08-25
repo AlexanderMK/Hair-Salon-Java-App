7e31e19baa93d110043db7ff853716ecdb0a1086
Alexander <mburukungu@gmail.com>
2017-08-25 16:10:13 +0300

    test2- check if stylist objects get a name property

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -3,9 +3,18 @@ import static org.junit.Assert.*;
 
 public class StylistTest {
 
+
+//check if tests will instantiate corretcly
   @Test
   public void stylist_instantiatesCorrectly_true() {
     Stylist testStylist = new Stylist("Xena");
     assertEquals(true, testStylist instanceof Stylist);
+  }
+
+//test if Stylist objects will get name property
+  @Test
+  public void getName_stylistInstantiatesWithName_Xena() {
+    Category testCategory = new Category("Xena");
+    assertEquals("Xena", testCategory.getName());
   }
 }
