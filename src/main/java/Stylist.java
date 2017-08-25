61778fd3b4c3ef32c28cbb13187da91f463de8bc
Alexander <mburukungu@gmail.com>
2017-08-25 16:46:54 +0300

    return method to give stylists unique ids

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 public class Stylist {
   private String mName;
   private static List<Stylist> instances = new ArrayList<Stylist>();
+  private int mId;
 
   //logic for initial instantiation
   public Stylist(String name) {
     mName = name;
     instances.add(this);
+    mId = instances.size();
   }
 
   //give a stylist a name
@@ -24,5 +26,10 @@ public class Stylist {
   //clear all instances of a stylist
   public static void clear() {
     instances.clear();
+  }
+
+  //getting id for a stylist
+  public int getId() {
+    return mId;
   }
 }
