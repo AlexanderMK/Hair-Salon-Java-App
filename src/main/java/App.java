60a758d6b2890dc3a59a2b08a92fb5b7fe0074da
Alexander <mburukungu@gmail.com>
2017-08-25 17:16:19 +0300

    placed route to create new form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -63,5 +63,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    //route to create new stylist
+    get("/stylists/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/stylist-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
   }
 }
