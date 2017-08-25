e1777a29c9da92610516705591918848965e2c8c
Alexander <mburukungu@gmail.com>
2017-08-25 16:41:44 +0300

    placed method to clear all stylist instances

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,16 +2,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Stylist {
-  private String name;
+  private String mName;
   private static List<Stylist> instances = new ArrayList<Stylist>();
 
   //logic for initial instantiation
   public Stylist(String name) {
     mName = name;
+    instances.add(this);
   }
 
   //give a stylist a name
   public String getName() {
     return mName;
+  }
+
+  //returns all instances of a stylist
+  public static List<Stylist> all() {
+    return instances;
+  }
+
+  //clear all instances of a stylist
+  public static void clear() {
+    instances.clear();
   }
 }
